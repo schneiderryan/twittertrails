11ef8c2532c508f08d511485c71494df1db91c6b
scpmrh <sean.reags@gmail.com>
2021-04-20 16:15:53 -0400

    Super basic server call
    getNLQ just makes a call to the backend

---
+++ server.py
@@ -0,0 +1,26 @@
+import json
+from ibm_watson import DiscoveryV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('o2AHBjAYfudK0mXXt7dFmQFx8oaldIae5oG3du2iIf9K')
+discovery = DiscoveryV1(
+    version='2019-04-30',
+    authenticator=authenticator
+)
+
+discovery.set_service_url('https://api.us-south.discovery.watson.cloud.ibm.com/instances/beff1375-93ea-4b19-b90a-10fbf38f45fd')
+
+envID = '35ef0ced-f8c5-4f16-a57c-098c66505472'
+colID = 'c7bf0198-9e14-40db-9e96-2b4d348585c1'
+
+def getNLQ(tweet: str, lat: float, lng: float):
+    areaFilter = '(latitude>' + str(eval("lat - .5")) + ',latitude<' + str(eval("lat - (.5) * -1")) + ',longitude>' + str(eval("(lng * 1) - .5")) + ',longitude<' + str(eval("(lng * 1) - (.5 * -1)")) + ')'
+    DetailedResponse = discovery.query(environment_id = envID, collection_id = colID, filter = str(areaFilter), natural_language_query = tweet, count = 10)
+    response = json.dumps(DetailedResponse.get_result(), indent = 2)
+    response = json.loads(response)
+    return response
+
+tweetResponse = getNLQ("walks around columbus", 40, 83)
+
+print(tweetResponse['results'][0]['name'])
+
